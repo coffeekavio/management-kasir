9d288e8a3125716e61942eff25a3b49551455cf5
Devsam_ <sabillyamri1@gmail.com>
2026-05-24 21:34:38 +0700

    fix : Mengubah ke mode Static Export untuk Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Mengubah Next.js menjadi Static HTML murni
+  images: {
+    unoptimized: true, // Wajib ditambahkan agar error gambar tidak muncul saat mode export
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Opsional: Mempercepat build dengan mengabaikan warning typo
+  }
+};
 
 export default nextConfig;
